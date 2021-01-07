774fec07c8c6bdbc749a5d36100cc884060aa415
amirbena <amir12061968@gmail.com>
2021-01-07 09:22:11 +0200

    First Commit to Master

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const routes = require('./routes');
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+/**
+ * Section for routes
+ */
+
+
+const PORT = process.env.PORT || 80;
+
+
+const server = app.listen(PORT, () => console.log(`Listenes to ${PORT} port`));
+
+module.exports = server;

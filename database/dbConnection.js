2c497452ef9f1625c5c6e9776e5bf35828bf1630
amirbena <amir12061968@gmail.com>
2021-01-08 14:11:02 +0200

    Change connect function

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 
-const connectToDB = async () => {
-    const dbConnection = process.env.MONGODB_URI || "mongodb://localhost:27017/herolo";
-    try {
-        await mongoose.connect(dbConnection, {
-            useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true
-        });
-        console.log("DB CONNECTED");
-        mongoose.Promise = global.Promise;
-        
-    } catch (error) {
-        console.error("Can't connect to db");
-    }
-}
 
-connectToDB();
+
+const dbConnection = process.env.MONGODB_URI || "mongodb://localhost:27017/herolo";
+
+mongoose.connect(dbConnection, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true
+}).then(()=>{
+    console.log("DB CONNECTED");
+    mongoose.Promise = global.Promise;
+}).catch(ex=>{
+    console.error("Can't connect to db ",ex);
+});
